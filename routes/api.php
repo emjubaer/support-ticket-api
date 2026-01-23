1e54b3d7f4489b472879ed349f5a0f85f08055b1
Jubaer <mdjubaer424@gmail.com>
2026-01-23 10:19:10 +0600

    Add initial route files, gitignore configurations, and basic tests
    - Created api.php for API routes including user authentication.
    - Added console.php for Artisan commands.
    - Established web.php for web routes.
    - Configured .gitignore files for storage directories to exclude unnecessary files.
    - Implemented basic feature and unit tests.
    - Set up Vite configuration for asset management with Laravel.

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function(){
+
+
+
+});
